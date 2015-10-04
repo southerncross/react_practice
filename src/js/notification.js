383c5238153bfd720c3eaa0b0e4c9cd95a4f5022
LiShunyang <lishunyang@pku.edu.cn>
2015-10-04 12:57:14 +0800

    Add communication between parent and child component.

--- src/js/notification.js
+++ src/js/notification.js
@@ -2,50 +2,94 @@ var React = require('react');
 
 var NotificationPanel = React.createClass({
     render: function () {
-        var departments = this.props.data.map(function(department) {
-            return <Department departmentName={department.name} positions={department.positions} />
+        var departments = this.props.data.map(function (department) {
+            return <Department departmentName={department.departmentName} positions={department.positions}/>
         });
 
         return (
             <div>
                 <p>招聘职位<span>清空</span></p>
-                <ul>
-                    {departments}
-                </ul>
+
+                {departments}
             </div>
         );
     }
 });
 
 var Department = React.createClass({
+    getInitialState: function () {
+        return {
+            checked: false
+        };
+    },
+
+    toggleChecked: function () {
+        var newChecked = !this.state.checked;
+        this.setState({
+            checked: newChecked
+        });
+        this.props.positions.forEach(function (position, index) {
+            this.refs['position-' + index].setChecked(newChecked);
+        }.bind(this));
+    },
+
     render: function () {
-        var notificationSum = this.props.positions.reduce(function(sum, position) {
+        var notificationSum = this.props.positions.reduce(function (sum, position) {
             return sum + position.notificationCount;
         }, 0);
-        var positions = this.props.positions.map(function(position) {
-            return <Position positionName={position.positionName} notificationCount={position.notificationCount} />
+        var positions = this.props.positions.map(function (position, index) {
+            return <Position
+                positionName={position.positionName}
+                notificationCount={position.notificationCount}
+                checked={position.checked}
+                ref={'position-' + index}
+                />
         });
 
         return (
-            <li>
-                <input type="checkbox"/>
-                <p>{this.props.departmentName}<span>{notificationSum}</span></p>
-                <ul>
-                    {positions}
-                </ul>
-            </li>
+            <div>
+                <input
+                    type='checkbox'
+                    checked={this.state.checked}
+                    onChange={this.toggleChecked}
+                    />
 
+                {this.props.departmentName}<span></span><span>{notificationSum}</span>
+
+                {positions}
+            </div>
         );
     }
 });
 
 var Position = React.createClass({
+    getInitialState: function () {
+        return {
+            checked: false
+        };
+    },
+
+    toggleChecked: function () {
+        this.setChecked(!this.state.checked);
+    },
+
+    setChecked: function (checked) {
+        this.setState({
+            checked: checked
+        })
+    },
+
     render: function () {
         return (
-            <li>
-                <input type="checkbox"/>
-                <p>{this.props.positionName}<span>{this.props.notificationCount}</span></p>
-            </li>
+            <div>
+                <input
+                    type='checkbox'
+                    checked={this.state.checked}
+                    onChange={this.toggleChecked}
+                    />
+
+                {this.props.positionName}<span>{this.props.notificationCount}</span>
+            </div>
         );
     }
 });
